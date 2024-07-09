f2d5a004f3601f33828c112d0a10760fc65fea13
kelly treskunov <kellytreskunov@gmail.com>
2024-07-09 07:47:52 +0000

    PROJECT1

---
+++ yourSolution.sh
@@ -0,0 +1,8 @@
+mkdir secretDir
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+chmod a-x secretDir/.secret
+rm -rf maliciousFiles
+chmod +x generateSecret.sh
+rm -rf important.link
+bash generatrSecret.sh
